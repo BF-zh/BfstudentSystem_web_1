f83e0acc4d6ae81b739048a4270dbeb8a88eb728
北风 <2581807417@qq.com>
2023-06-23 10:54:27 +0800

    完成了登陆注册功能

---
+++ src/apis/index.ts
@@ -0,0 +1,13 @@
+export const ACCOUNT = {
+  REIGITURL: '/api/auth/register',
+  VALID_REGISTER_EMAIL: '/api/auth/valid-register-email',
+  LOGIN: '/api/auth/login',
+  LOGOUT: '/api/auth/logout',
+  VALID_RESET_EMAIL: '/api/auth/valid-reset-email',
+  START_RESET: '/api/auth/start-reset',
+  DO_RESET: '/api/auth/do-reset',
+
+}
+export const USER = {
+  INFO: '/api/user/me',
+}
---
+++ unocss.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, presetAttributify, presetIcons, presetUno, presetWind, transformerDirectives } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetWind(),
+    presetIcons({
+      extraProperties: {
+        'display': 'inline-block',
+        'vertical-align': 'middle',
+        'width': '2em',
+        'height': '2em',
+      },
+    }),
+    presetAttributify(),
+  ],
+  transformers: [transformerDirectives()],
+  rules: [
+    [/^(?:line|l)-(?:h|height)-(.+)$/, ([,height]) => ({ 'line-height': `${height}` })],
+    [/^(?:line|l)-(?:h|height)-(\d+)$/, ([,height]: any) => ({ 'line-height': `${height / 4}em` })],
+  ],
+})
